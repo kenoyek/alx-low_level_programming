cfde7ed92be3bceb73f65d03b8bb75f41709cd47
kenoyek <kenoyek@gmail.com>
2022-09-15 12:39:44 +0100

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
+
 /**
- * print_numbers - Prints the number zero to nine
- *
- * Return: Always 0 (Success)
- */
+  * print_numbers - Print numbers from 0 - 9
+  * Description: Use only _putchar twice
+  * Return: void
+  */
 void print_numbers(void)
 {
-char num;
+	int num;
 
-for (num = '0'; num <= '9'; num++)
-{
-_putchar(num);
-}
-_putchar('\n');
-
-return;
+	for (num = 48; num <= 57; num++)
+		_putchar(num);
+	_putchar('\n');
 }
